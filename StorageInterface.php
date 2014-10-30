805ed74ab4a5ad33c4e98b9887189505e035a8fe
callmez <callme-z@qq.com>
2014-10-30 23:30:46 +0800

    ...

--- StorageInterface.php
+++ StorageInterface.php
@@ -1,19 +1,8 @@
 <?php
+
 namespace callmez\storage;
 
-use yii\base\Component;
-
-abstract class BaseStorage extends Component
-{
-//    abstract public function upload();
-//    abstract public function isUpload();
-//    abstract public function uploadValidate();
-//    abstract public function saveFileByUrl($url, $saveName = null);
-//    abstract public function saveFileByPath($path, $saveName = null);
-//    abstract public function saveFileByContent($content, $saveName = null);
-}
-
-interface StorageInterface extends ReadInterface
+interface AdapterInterface extends ReadInterface
 {
     /**
      * @const  VISIBILITY_PUBLIC  public visibility
